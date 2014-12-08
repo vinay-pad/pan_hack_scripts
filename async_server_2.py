1459ca21c27df17af7ce051d413075bfc15c196c
Vinay Padmanabhi <vin2502@gmail.com>
2014-12-07 23:48:32 -0800

    Final scripts

--- async_server_2.py
+++ async_server_2.py
@@ -73,6 +73,9 @@ class EchoHandler(asyncore.dispatcher_with_send):
                 "X-Parse-REST-API-Key": "RAIna9hI5q5OjxzQzbgq00rYrnOnrMAovCEfwiIb",
                 "Content-Type": "application/json"
          })
+	
+	req = "\"http://10.5.104.198/api/?type=config&action=set&key=LUFRPT14MW5xOEo1R09KVlBZNnpnemh0VHRBOWl6TGM9NUZhL25kL29vMlRaaXp4WGFOQUI5UT09&xpath=/config/devices/entry[@name='localhost.localdomain']/vsys/entry[@name='vsys1']/rulebase/security/rules/entry[@name='block-threat']&element=<source><member>"+str(threat_source)+"</member></source><description>Policy to block threat</description><action>deny</action>\""
+        os.system('wget -O vinay.com '+req)
 	print 'Sent threat alert to Parse'
 
     def __parse_sys(self, root):
@@ -99,6 +102,68 @@ class EchoHandler(asyncore.dispatcher_with_send):
          })
 	print 'Sent system alert to Parse'
 	
+    def __parse_info(self, root):
+	#cpu entries
+	ip = root.attrib['ip-addr']
+	name = root.attrib['name']
+	cpu_entries = root.find("cpu-usage").findall("entry")
+	total_cpu = cpu_entries[0].find("value").text
+	mgmtsrvr_cpu = cpu_entries[1].find("value").text
+        logrcvr_cpu = cpu_entries[2].find("value").text
+
+	#disk entries
+	disk_entries = root.find("disk-usage").findall("entry")
+	opt_pancfg_usage = disk_entries[0].find("usage").text
+	opt_panrepo_usage = disk_entries[1].find("usage").text
+	dev_shm_usage = disk_entries[2].find("usage").text
+	opt_panlogs_usage = disk_entries[3].find("usage").text
+
+	#memory usage
+	mem_entries = root.find("memory-usage").findall("entry")
+	total_mem = mem_entries[0].find("value").text
+	mgmtsrvr_mem = mem_entries[1].find("value").text
+	logrcvr_mem = mem_entries[2].find("value").text
+
+	#lograte
+	lograte = root.find("log-rate").text
+
+	#throughput
+	throughput = root.find("throughput").text
+
+	#active sessions
+	active_sess = root.find("active-sessions").text
+
+	#ha_status
+	ha_status = root.find("ha-status").text
+	conn_status = root.find("conn_status").text
+	
+	connection = httplib.HTTPSConnection('api.parse.com', 443)
+        connection.connect()
+        connection.request('POST', '/1/functions/UPDATE_INFO_STATUS', json.dumps({
+                "name":name,
+                "total_cpu": total_cpu,
+                "mgmtsrvr_cpu" : mgmtsrvr_cpu,
+		"logrcvr_cpu" : logrcvr_cpu,
+                "ip": ip,
+                "opt_pancfg_usage" : opt_pancfg_usage,
+                "opt_panrepo_usage" : opt_panrepo_usage,
+                "dev_shm_usage" : dev_shm_usage,
+                "opt_panlogs_usage" : opt_panlogs_usage,
+                "total_mem": total_mem,
+                "mgmtsrvr_mem" : mgmtsrvr_mem,
+		"logrcvr_mem" : logrcvr_mem,
+                "lograte" : lograte,
+                "throughput" : throughput,
+                "active_sess" : active_sess,
+                "ha_status" : ha_status,
+		"conn_status" : conn_status
+                }), {
+                "X-Parse-Application-Id": "RnsDdfZemEz0bGQoqMQE4JyyfSLcdJmqTreY5QD9",
+                "X-Parse-REST-API-Key": "RAIna9hI5q5OjxzQzbgq00rYrnOnrMAovCEfwiIb",
+                "Content-Type": "application/json"
+         })
+        print 'Sent info alert to Parse'
+
     def __parse_xml(self, xml_str):
 	tree = ET.fromstring(xml_str)
 	
@@ -106,6 +171,8 @@ class EchoHandler(asyncore.dispatcher_with_send):
 		self.__parse_threat(tree)		
 	elif tree.tag == 'firewall-system-alert':
 		self.__parse_sys(tree)
+	elif tree.tag == 'firewall-sysinfo':
+		self.__parse_info(tree)
 	
 	
 	
